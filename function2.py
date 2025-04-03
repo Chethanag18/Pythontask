2002804255cbfb752fb96854b47f62b2cb221b47
chethanag18 <chethanag18.xworkz@gmail.com>
2025-04-03 21:29:30 +0530

    file added

---
+++ function2.py
@@ -0,0 +1,8 @@
+def get_details(name,age,address):
+    print("The name of the person is",name)
+    print("The age of the person is",age)
+    print("The address of the person is",address)
+person_name = input()
+person_age = int(input())
+person_address = input()
+get_details(person_name,person_age,person_address)
