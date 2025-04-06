0c446898ca3a45af32af5d1a212b37d623ac1444
chethanag18 <chethanag18.xworkz@gmail.com>
2025-04-06 10:32:20 +0530

    add file

--- passwordmatch.py
+++ passwordmatch.py
@@ -1,5 +1,5 @@
 import re 
-pattern = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!]).{6,}$'
+pattern =  r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!]).{6,}$'
 password = input()
 if re.fullmatch(pattern,password):
     print("Valid Password")
